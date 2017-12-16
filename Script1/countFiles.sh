43df2b485459aa7c2231798aa86a7b9b92aab0f5
Divyansh <divi09112@gmail.com>
2017-12-17 03:10:25 +0530

    trial4

--- Script1/countFiles.sh
+++ Script1/countFiles.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-ls -al "$1" | grep -o ^\-.*$2$ | wc -l
+#COULD FAIL IF EXTENSION CONTAINS REGEX EXPRESSIONS BECAUSE $2 IS EXPANDED BEFORE REGEX
+
+if [ "$1" = '' ] || [ ! -d "$1" ];
+then
+	echo "Invalid Directory"
+	exit 1
+else
+	ls -al "$1" | grep -oc "^\-.*$2$"
+fi
