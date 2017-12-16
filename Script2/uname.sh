bcc0faeff3a1ab0747aaf41b0e11acca601b49c0
Divyansh <divi09112@gmail.com>
2017-12-17 04:43:30 +0530

    trial2 without error handling

--- Script2/uname.sh
+++ Script2/uname.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+#FAILS IF USERNAME HAS SPACES
+
 count=0
 
 line=$(cat "$1" | grep "$2" | tr ":" " ")
 
-for i in $line; do
-	count=$(($count+1))
-	if [ $count = 5 ];
-	then
-		echo $i
-	fi
-done
+len=$(echo $line | wc -w)
+
+if [ $len -lt 7 ]; 
+then
+	echo "Invalid Username"
+else
+	for i in $line; do
+		count=$(($count+1))
+		if [ $count = 5 ];
+		then
+			echo $i
+		fi
+	done
+fi
