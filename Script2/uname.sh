3de44d18dd7aad446dd1def487fd37fc52e3faa5
Divyansh <divi09112@gmail.com>
2017-12-17 11:30:14 +0530

    trial3 without error handling

--- Script2/uname.sh
+++ Script2/uname.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
-#FAILS IF USERNAME HAS SPACES
-
-count=0
-
-line=$(cat "$1" | grep "$2" | tr ":" " ")
-
-len=$(echo $line | wc -w)
-
-if [ $len -lt 7 ]; 
-then
-	echo "Invalid Username"
-else
-	for i in $line; do
-		count=$(($count+1))
-		if [ $count = 5 ];
-		then
-			echo $i
-		fi
-	done
-fi
+cat "$1" | grep "^$2" | cut -d":" -f5
