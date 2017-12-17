a2027e986b078d8f4f24abd5cc2dad3d1994d35f
Divyansh <divi09112@gmail.com>
2017-12-18 02:56:50 +0530

    final code with proper error handling

--- Script1/countFiles.sh
+++ Script1/countFiles.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-#COULD FAIL IF EXTENSION CONTAINS REGEX EXPRESSIONS BECAUSE $2 IS EXPANDED BEFORE REGEX
+#FINAL CODE WITH PROPER ERROR HANDLING
 
-if [ "$1" = '' ] || [ ! -d "$1" ];
-then
+if [ "$1" = '' ] || [ ! -d "$1" ];then
 	echo "Invalid Directory"
 	exit 1
 else
-	ls -al "$1" | grep -oc "^\-.*$2$"
+	if [ "$2" = "" ];then
+		ls -al "$1" | grep -oc "^\-.*$"
+	elif [[ "$2" =~ ^\.[a-z]+$ ]];then
+		ls -al "$1" | grep -oc "^\-.*$2$"
+	else
+		echo "Invalid extension"
+		exit 1
+	fi
 fi
