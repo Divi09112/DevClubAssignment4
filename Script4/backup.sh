ece5e30fe804cefd3642e5dd29ea93611e364aaf
Divyansh <divi09112@gmail.com>
2017-12-17 19:17:48 +0530

    basic logic

--- Script4/backup.sh
+++ Script4/backup.sh
@@ -3,18 +3,13 @@
 #flow control for root directories only
 
 for file in $(ls "$1");do
-	echo $file
-	if [ -e "$2"/$file ] ;then
-		echo "yes"
-	else
-		echo "no"
+	if [ ! -e "$2"/$file ] ;then
+		cp -r "$1"/$file "$2"
 	fi
 done
 
 for file in $(ls "$2");do
-	if [ -e "$1"/$file ] ;then
-		echo "yes"
-	else
-		echo "no"
+	if [ ! -e "$1"/$file ] ;then
+		cp -r "$2"/$file "$1"
 	fi
 done
