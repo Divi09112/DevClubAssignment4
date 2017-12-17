41e161db43471d6be6d668300ac77e2a47ea9820
Divyansh <divi09112@gmail.com>
2017-12-17 11:42:43 +0530

    trial4

--- Script2/uname.sh
+++ Script2/uname.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-cat "$1" | grep "^$2" | cut -d":" -f5
+#DOESN'T CHECK IF FILE IS OF THE REQUIRED FORMAT AND FAILS IF INITIAL PART OF THE USER IS GIVEN
+
+if [ $1 = '' ] || [ ! -f "$1" ];
+then
+	echo "Invalid File"
+	exit 1
+elif [ "$2" = '' ];
+then
+	echo "Invalid user"
+	exit 1
+else
+	line=$(cat "$1" | grep "^$2")
+	if [ "$line" = '' ];
+	then
+		echo "User not present"
+	else
+		echo "$line" | cut -d":" -f5
+	fi
+fi
+
+
