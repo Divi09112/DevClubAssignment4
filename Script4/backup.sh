286b3424d357163d5c063e3b509bea6a2f4b22ff
Divyansh <divi09112@gmail.com>
2017-12-18 13:42:38 +0530

    error handling added

--- Script4/backup.sh
+++ Script4/backup.sh
@@ -3,16 +3,34 @@
 #final code without error handling
 
 
+declare -r org="$PWD"
+
 backup () {
 for file in $(ls "$1");do
+
 	if [ ! -e "$2"/$file ];then
 		cp -r "$1"/$file "$2"
+
 	elif [ -d "$1"/$file ] && [ -e "$2"/$file ];then
 		backup "$1"/$file "$2"/$file
+
 	fi
 done
 
 }
 
-backup "$1" "$2"
-backup "$2" "$1"
+
+if [ ! "$#" -eq 2 ]; then
+
+	echo "Insufficient arguements"
+	exit 1
+
+elif [ ! -d "$1" ] || [ ! -d "$2" ]; then
+
+	echo "Not a valid directory"
+	exit 1
+
+else
+	backup "$1" "$2"
+	backup "$2" "$1"
+fi
